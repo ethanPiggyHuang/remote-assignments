ace1e82bd486303453340a488ae2d0d09b624093
黃怡諶 <ethanhuang@huangyichendeMacBook-Pro.local>
2023-01-22 22:36:36 +0800

    add assignment5

--- week-3/app.js
+++ week-3/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');        
+const cookieParser = require('cookie-parser');      
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 app.use(express.static('public'));
 
 app.set('view engine', 'pug');
@@ -27,6 +31,35 @@ app.get('/getData', (req, res) => {
   res.render('getData', { number, sum, message});
 });
 
+app.get('/myName', (req, res) => {
+  const { name } = req.cookies;
+  if (name){
+    res.render('myName', { name });
+  } else {
+    res.redirect('/trackName');
+  }
+})
+
+app.get('/trackName', (req, res) => {
+  let name='';
+  if (req.query.name)       //input name by route query
+  {
+    name = req.query.name;
+    res.cookie('name', req.query.name);
+    res.redirect('/myName');
+  } else if (req.cookies.name)   //check if there is cookie for name
+  {
+    name = req.cookies.name;
+    res.redirect('/myName');
+  } else {
+    res.render('trackName');
+  }
+});
+
+app.post('/trackName', (req, res) => {
+  res.cookie('name', req.body.name);   //input name by cookie
+  res.redirect('/myName');
+});
 
 app.listen(3000, () => {
   console.log('The application is running on localhost: 3000');
