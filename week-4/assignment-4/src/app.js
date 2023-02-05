67cc5eb2dd702f8ec7495bb41d12a351eb0c4074
黃怡諶 <ethanhuang@huangyichendeMacBook-Pro.local>
2023-02-05 22:29:10 +0800

    add assignment 4

---
+++ week-4/assignment-4/src/app.js
@@ -0,0 +1,72 @@
+const url = 'https://api.github.com/orgs/facebook/repos?per_page=5&amp;page=';
+
+function ajax(url) {
+  return(
+    fetch(url)
+      .then( (response) => response.text())
+      .then( data  => JSON.parse(data) )
+  );    
+}
+
+function render(data) {
+  
+  class App extends React.Component{
+    state = {
+      display: data,
+      pageNum: 1
+    }
+    
+    fetchData = (pageNum) => {
+      pageNum += 1;
+      const newUrl = url + pageNum;
+
+      ajax(newUrl).then((data) => {
+        this.setState( prevState => {
+          prevState.display.push(...data);
+          return { 
+            display: prevState.display
+          };
+        });
+      });
+
+      this.setState( prevState => {
+        return { 
+          pageNum: prevState.pageNum + 1
+        };
+      });
+    }    
+
+    render () {
+      return(
+        <div className="outer-container">
+          <div className="flex-container">
+            {this.state.display.map( product =>
+              <div className="flex-item">
+                <div className="inner-row">
+                  <p id="name">{ product.name }</p>
+                  <p id="visibility">{ product.visibility }</p>
+                </div>
+                <p id="description">{ product.description }</p>
+                <div className="inner-row">
+                  {product.topics.map( topic => <p id="topic">{ topic }</p>)}
+                </div>
+              </div>
+            )}
+          </div>
+          <button onClick = {() => this.fetchData(this.state.pageNum)}>more</button>
+        </div>
+      );
+    }
+  }
+
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  )
+}
+  
+const initUrl = url + 1;
+
+ajax(initUrl).then((data) => {
+  render(data);
+});
